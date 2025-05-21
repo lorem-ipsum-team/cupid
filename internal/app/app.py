530d27bdddda8f8ef8947514d562fe491dbb5320
Antttezy <antkumachev@gmail.com>
2025-05-21 13:21:18 +0300

    Hotfix: allow CORS from localhost:3000

--- internal/app/app.py
+++ internal/app/app.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from internal.router import api_router, health
 from internal.config.settings import settings
 
 
 def create_app():
     app = FastAPI(title="Cupid")
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=['http://localhost:3000'],
+        allow_methods=['*'],
+        allow_headers=['*']
+    )
     app.include_router(api_router, prefix=settings.API_PREFIX)
     app.include_router(health.router, prefix=settings.HEALTH_PREFIX)
 
