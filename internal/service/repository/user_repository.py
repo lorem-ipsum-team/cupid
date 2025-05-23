6d89f481cea46c36875bbadc67841ec71d33834c
Antttezy <antkumachev@gmail.com>
2025-05-23 17:18:12 +0300

    Try optimizing by splitting query

--- internal/service/repository/user_repository.py
+++ internal/service/repository/user_repository.py
@@ -13,19 +13,23 @@ class UserRepository:
 
     async def get_user_data_by_id(self,
                                   user_id: UUID) -> Tuple[User, UserPreference, UserGeo]:
-        query = select(User, UserPreference, UserGeo)\
-            .join(UserPreference, User.id == UserPreference.id, isouter=True)\
-            .join(UserGeo, User.id == UserGeo.id, isouter=True)\
+        user_query = select(User)\
             .where(User.id == user_id)\
             .limit(1)
 
-        result = await self.__db.execute(query)
-        row = result.one_or_none()
+        pref_query = select(UserPreference)\
+            .where(UserPreference.id == user_id)\
+            .limit(1)
 
-        if row is None:
-            return None, None, None
+        geo_query = select(UserGeo)\
+            .where(UserGeo.id == user_id)\
+            .limit(1)
 
-        return row
+        user = await self.__db.scalars(user_query)
+        pref = await self.__db.scalars(pref_query)
+        geo = await self.__db.scalars(geo_query)
+
+        return user.one_or_none(), pref.one_or_none(), geo.one_or_none()
 
     async def get_nearby_users(self, user: User, geo: UserGeo, limit: int, radius: float):
         query = select(User, UserPhoto, UserDescription)\
